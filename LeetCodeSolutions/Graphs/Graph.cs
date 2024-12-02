7a2a2ea40cf9d4f4d738fb13d9c01dfdcf218e01
Gecher Emre <emre.gecher@indusoft.ru>
2024-12-02 14:17:24 +0300

    test

--- LeetCodeSolutions/Graphs/Graph.cs
+++ LeetCodeSolutions/Graphs/Graph.cs
@@ -8,49 +8,7 @@ namespace LeetCodeSolutions.Graphs
 {
     public class Graph
     {
-        // Düğüm ile komşuluk listesini tutan yapı
-        private Dictionary<int, List<int>> adjacencyList;
-
-        public Graph()
-        {
-            adjacencyList = new Dictionary<int, List<int>>();
-        }
-
-        // Graf'a düğüm ekleme
-        public void AddVertex(int vertex)
-        {
-            if (!adjacencyList.ContainsKey(vertex))
-            {
-                adjacencyList[vertex] = new List<int>();
-
-            }
-        }
-
-        // Düğümler arası kenar ekleme
-        public void AddEdge(int vertex1, int vertex2)
-        {
-            if (adjacencyList.ContainsKey(vertex1) && adjacencyList.ContainsKey(vertex2))
-            {
-                adjacencyList[vertex1].Add(vertex2);
-                adjacencyList[vertex2].Add(vertex1);// Yönsüz graf olduğu için her iki tarafa da ekliyoruz
-
-            }
-
-        }
-
-        public void PrintGraph()
-        {
-            foreach (var vertex in adjacencyList)
-            {
-                Console.WriteLine($"{vertex.Key}");
-                foreach (var neighbor in vertex.Value)
-                {
-                    Console.WriteLine($"{neighbor}");
-
-                }
-                Console.WriteLine();
-            }
-        }
+        
 
         #region ExampleUsage
         /*
