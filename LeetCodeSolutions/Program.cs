d2b1463e9654421697b58996cc5b20b95e784eb8
Gecher Emre <emre.gecher@indusoft.ru>
2024-12-03 15:35:31 +0300

    update
    update
    
    insertion sort added
    
    update
    
    sort func update

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using LeetCodeSolutions.InterviewQuestions;
 using LeetCodeSolutions.Search;
+using LeetCodeSolutions.Sorting.Insertion;
 using LeetCodeSolutions.SortingAlgorithms.Merge;
 using LeetCodeSolutions.Tasks;
 
@@ -10,67 +11,19 @@ namespace LeetCodeSolutions
         static void Main(string[] args)
         {
 
-           int[] list = { 8, 3, 5, 1, 4, 7, 6, 2 };
+            List<int> list = [ 8, 3, 5, 1, 4, 7, 6, 2 ];
 
-          MergeSorting.MergeSort(list, 0, list.Length-1);
-          //BinarySearch binarySearch = new BinarySearch();
+         // MergeSorting.MergeSort(list, 0, list.Length-1);
+            //BinarySearch binarySearch = new BinarySearch();
 
-          //  int[] sortedArray = { 1, 3, 5, 7, 9, 11, 13, 15 };
-          //  int target = 15;
+            //  int[] sortedArray = { 1, 3, 5, 7, 9, 11, 13, 15 };
+            //  int target = 15;
+            List<int> array = [2,4,6,8,3];
 
-          //  int index = binarySearch.Search(sortedArray, target);
-          //  Console.WriteLine(index); 
-
-            //int[] arr = { 4, 4, 2, 3, 4, 4 };
-            //int[] arr2 = { 2, 7, 11, 15 };
-            //int target = 9;
-            //string tr = "123";
-            //string ms = "SAD123";
-
-            //Console.WriteLine(ms.IndexOf(tr));
-
-            //int[] arr = { 1, 2, 3, 4, 5, 6 };
-            //int target = 3;
-            //Stopwatch sw = new Stopwatch();
-            //sw.Start();
-            //Console.WriteLine(SearchInser(arr, target));
-            //sw.Stop();
-            // Console.WriteLine(sw.Elapsed);
-
-            //int k = RemoveDuplicates(arr);
-            //int k = RemoveElement(arr,4);
-
-            //int[] result = TwoSum(arr2, target);
-            //Console.WriteLine("Indices of the two numbers that add up to the target are: [{0}, {1}]", result[0], result[1]);
-
-            //Console.WriteLine("k = " + k); // Output: k = 5
-            ////Console.WriteLine("Modified array:");
-
-            //for (int i = 0; i < k; i++)
-            //{
-            //    Console.Write(arr[i] + " "); // Output: 0 1 2 3 4 
-            //}
-            //int[] arr = { 1, 2, 3, 4, 9 };
-
-            //foreach (var item in PlusOne(arr))
-            //{
-            //    Console.Write(item);
-            //}
-
-            //string str = "csharpcorner";
-
-            //RemoveDublicateChar.RemoveDublicate(str);
-
-            //int[] arr = { 4, 4, 2, 3, 4, 4 };
-            //int[] arr2 = { 2, 7, 11, 15 };
-            //ArrayMerge.Merging(arr, arr2);
-
-            //string time = "12:01:00AM";
-            //Console.Write(TimeConversion.Convert(time));
-
-            //List<int> arr = new List<int>{ 1, 3, 5, 6, 4, 6 };
-            //MiniMaxSum.minMax(arr)
-
+            InsertionSort.SortFromTheEnd();
+                                                
+         
+            
         }
 
 
--- LeetCodeSolutions/Sorting/Insertion/InsertionSort.cs
+++ LeetCodeSolutions/Sorting/Insertion/InsertionSort.cs
@@ -1,12 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace LeetCodeSolutions.Sorting.Insertion
+namespace LeetCodeSolutions.Sorting.Insertion;
+
+public static class InsertionSort
 {
-    internal class InsertionSort
+    /// <summary>
+    /// First member of the list or array accepts as sorted
+    /// From the send member of list compears with the other members. 
+    /// Time complexity  O(n²)
+    /// </summary>
+    /// <param name="list"></param>
+    public static void Sort(List<int> list)
     {
+        //int[] array = { 12, 11, 13, 5, 6 }; 
+
+        int lenght = list.Count;
+        int target = list.Count - 1;
+
+        for (int i = 1; i < lenght; i++)
+        {
+            int key = list[i]; //The current number
+            int j = i - 1;
+
+            //Put the key in to subset 
+            while (j >= 0 && list[j] > key)
+            {
+                list[j + 1] = list[j];
+                j--;
+            }
+            list[j + 1] = key; // Put the key to right place
+
+        }
+
+
+        Print(list);
+    }
+
+
+    public static void Print(List<int> list)
+    {
+        foreach (var item in list)
+        {
+            Console.WriteLine($"Sorted List: {item}");
+        }
+
+    }
+
+    public static void SortFromTheEnd()
+    {
+        List<int> arr = [2, 4, 6, 8, 3];
+        int n = arr.Count; //Dizi boyutu
+
+        int valueToInsert = arr[n - 1];//sagdaki en uc degeri kaydet
+        int i = n - 2;
+
+        while (i >= 0 && arr[i] > valueToInsert)
+        {
+            arr[i+1] = arr[i];
+            PrintArray(arr);
+            i--;
+        }
+        arr[i+1] = valueToInsert;
+        PrintArray(arr);
+    }
+    static void PrintArray(List<int> arr)
+    {
+        Console.WriteLine(string.Join(" ", arr));
     }
 }
